75124084d6d92ff952771dd3aa057757e18115c7
Jason Buckenham <jason.buckenham@aexp.com>
2020-07-24 17:53:42 +0100

    update list status added

--- app.py
+++ app.py
@@ -14,7 +14,20 @@ def get_todo_list():
 @app.route('/create', methods=['POST'])
 def create():
     session.add_item(request.form['add_todo'])
-    return get_todo_list()
+    return redirect('/')
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    for id in request.form:
+        item = session.get_item(id)
+        new_status = request.form.get(id)
+        item['status'] = new_status
+        session.save_item(item)
+    return redirect('/')
+    # I would add an if statement in this route to check status value
+    # and if status was 'delete' would trigger new delete function
+    # else it would update as above
 
 
 if __name__ == '__main__':
