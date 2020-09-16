7052a925fe8099503ff17f4c09920744372dcb0a
Jason Buckenham <jb@MacBook-Pro.lan>
2020-09-16 14:02:53 +0100

    new test app

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import pytest, dotenv, app
+
+
+@pytest.fixture
+def client():
+    file_path = dotenv.find_dotenv('.env.test')
+    dotenv.load_dotenv(file_path, override=True)
+    test_app = app.create_app()
+    with test_app.test_client() as client:
+        yield client
+
+        
+def test_index_page(mock_get_requests, client):
+    response = client.get('/')
+ 
