524d56ae5ca690be32b6eaeee3f9b6f325747ba5
Jason Buckenham <jb@MacBook-Pro.lan>
2021-02-01 08:06:33 +0000

    run all tests

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -3,5 +3,5 @@
 poetry run pytest todo_app/tests/test_todos.py
 wait
 poetry run pytest todo_app/tests/test_app.py
-# wait
-# poetry run pytest todo_app/browser_tests/chrome_test.py
+wait
+poetry run pytest todo_app/browser_tests/chrome_test.py
