02502245512a8e255569ab6f6cea6b8cf8eb953d
Jay Bee <jay.bee71@outlook.com>
2021-02-28 17:40:16 +0000

    step 1 & 2

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jaybee1971/todo_app:${TRAVIS_COMMIT} 
+docker push jaybee1971/todo_app:"$TRAVIS_COMMIT" 
